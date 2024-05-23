7ce41ce96d691bbb98e5c0f97cc1f1a69829d733
Hy <hyhuynh222@gmail.com>
2024-05-23 09:39:36 -0700

    Added first look of a working build of 2 room interaction, challenge, item requirements, inventory

--- Escape-the-Mad-Wizards-Lair/src/main/java/org/example/Room.java
+++ Escape-the-Mad-Wizards-Lair/src/main/java/org/example/Room.java
@@ -5,11 +5,18 @@ import java.util.ArrayList;
 public class Room {
     private final String name;
     private String description;
-    private ArrayList<String> items = new ArrayList<>();
+    private String outCome;
+    private String items;
     private ArrayList<String> requiredItems = new ArrayList<>();
     private String hints;
     private ArrayList<String> nextRooms = new ArrayList<>();
     private ArrayList<String> preRoomS = new ArrayList<>();
+    private Boolean clear = false;
+
+    private String challenge;
+    private String answer;
+
+    private ArrayList<String> actions = new ArrayList();
 
     public Room(String name){
         this.name = name;
@@ -25,17 +32,25 @@ public class Room {
     public void setDescription(String description){
         this.description = description;
     }
-    public void setItems(String item){
-        this.items.add(item);
+
+    public String getOutCome() {
+        return outCome;
     }
-    public ArrayList<String> getItems(String item){
+
+    public void setOutCome(String outCome){
+        this.outCome = outCome;
+    }
+    public void setItems(String item){
+        this.items = (item);
+    }
+    public String getItems(){
         return this.items;
     }
 
     public void setRequiredItems(String item){
         this.requiredItems.add(item);
     }
-    public ArrayList<String> getRequiredItems(String item){
+    public ArrayList<String> getRequiredItems(){
         return this.requiredItems;
     }
 
@@ -60,6 +75,39 @@ public class Room {
     public ArrayList<String> getPreRoom(){
         return this.preRoomS;
     }
+
+    public Boolean isCleared(){
+        return this.clear;
+    }
+
+    public void cleared(){
+        this.clear = true;
+    }
+
+    public void setActions(String action){
+        this.actions.add(action);
+    }
+
+    public ArrayList<String> getActions(){
+        return this.actions;
+    }
+
+    public void setChallenge(String challenge) {
+        this.challenge = challenge;
+    }
+
+    public String getChallenge() {
+        return this.challenge;
+    }
+
+    public void setAnswer(String challenge) {
+        this.answer = challenge;
+    }
+
+    public Boolean getAnswer(String answer) {
+        return (answer.equalsIgnoreCase(this.answer));
+    }
+
 
 
 }
--- Escape-the-Mad-Wizards-Lair/src/main/java/org/example/WorldMap.java
+++ Escape-the-Mad-Wizards-Lair/src/main/java/org/example/WorldMap.java
@@ -14,30 +14,97 @@ public class WorldMap {
     Map <String, Room> mapper = new HashMap<>();
     Room currentRoom = start;
 
+    Item player = new Item();
+
     BufferedReader getInput = new BufferedReader( new InputStreamReader(System.in));
     String input;
 
-    public void startMap() throws IOException {
+    public void startMap() throws IOException, InterruptedException {
         start.addNextRoom("Potion Room");
+        start.setDescription("You're in prison boi");
+        start.setActions("1) Pick Up Key\n 2) Cancel");
+        start.setChallenge("You noticed a man, well... half of a man talking to you. He told you that the only way to get out of this prison is to answer his question\n"+
+                           "\nQuestion: What is 1 + 1\n1) 1\n2) 2\n3) 3");
+        start.setAnswer("2");
+        start.setOutCome("He rewarded you with a Prison Key, and died * look at what you did *\n");
+        potionRoom.setRequiredItems("Prison Key");
+        start.setItems("Prison Key");
+        potionRoom.addPreRoom("Starting Room");
+        potionRoom.setDescription("You're in potion room boi");
         mapper.put(start.getName(), start);
         mapper.put(potionRoom.getName(), potionRoom);
-        potionRoom.addPreRoom("Starting Room");
 
         while(true){
+            Thread.sleep(1000);
             System.out.println(currentRoom.getName());
-            if (!(currentRoom.getNextRoom().isEmpty())){
-                System.out.println("You can go to forward:");
-                currentRoom.getNextRoom().forEach(System.out::println);
+            if (!(currentRoom.isCleared())){
+                System.out.println(currentRoom.getDescription());
             }
-            if (!(currentRoom.getPreRoom().isEmpty())){
-                System.out.println("You can go backward to:");
-                currentRoom.getPreRoom().forEach(System.out::println);
-            }
+            System.out.println("What would you like to do?");
+            System.out.println("1) Interact\n2) Move on\n3) Check Inventory");
             input = getInput.readLine();
+            switch (input){
+                case "1":
+                    if (currentRoom.isCleared()){
+                        System.out.println("Nothing else to see here");
+                    } else {
+                        System.out.println(start.getChallenge());
+                        while(true){
+                            input = getInput.readLine();
+                            if (currentRoom.getAnswer(input)){
+                                if(!(currentRoom.getItems().isEmpty())){
+                                    player.giveItem(currentRoom.getItems());
+                                    System.out.println(currentRoom.getOutCome());
+                                }
+                                break;
+                            } else {
+                                System.out.println("WRONG");
+                            }
+                        }
+                    }
+                    break;
+                case "2":
+                    while(true){
+                        try {
+                            if (!(currentRoom.getNextRoom().isEmpty())){
+                                System.out.println("You can go to forward:");
+                                currentRoom.getNextRoom().forEach(System.out::println);
+                            }
+                            if (!(currentRoom.getPreRoom().isEmpty())){
+                                System.out.println("You can go backward to:");
+                                currentRoom.getPreRoom().forEach(System.out::println);
+                            }
+                            input = getInput.readLine();
+                            if (mapper.get(input).getRequiredItems().isEmpty()){
+                                currentRoom = mapper.get(input);
+                                break;
+                            } else {
+                                for (String item: mapper.get(input).getRequiredItems()){
+                                    if (!player.hasItem(item)){
+                                        System.out.println("Sorry You can't continue, missing item(s)\n");
+                                        break;
+                                    } else {
+                                        currentRoom.cleared();
+                                        currentRoom = mapper.get(input);
+                                        break;
+                                    }
+                                }
+                                break;
+                            }
+                        } catch (NullPointerException e){
+                            System.out.println("That's not right, spell it right dummy\n");
+                        }
+                    }
+                    break;
+                case "3":
+                    player.getInventory();
+                    System.out.println("\n");
+
+            }
+
             if (input.equals("q")){
                 break;
             }
-            currentRoom = mapper.get(input);
         }
 
 
